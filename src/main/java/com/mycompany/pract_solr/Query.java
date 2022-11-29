ecdb0775d23ad336683704df400ab129da8c8b3b
antonio diego <antonio diego@DESKTOP-2LM465D>
2022-11-29 20:27:58 +0100

    Consultas creadas(bien hechas)

--- src/main/java/com/mycompany/pract_solr/Query.java
+++ src/main/java/com/mycompany/pract_solr/Query.java
@@ -5,16 +5,18 @@
 package com.mycompany.pract_solr;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocumentList;
-
 
 /**
  *
@@ -24,37 +26,45 @@ public class Query {
 
     public static void main(String[] args) throws IOException, SolrServerException {
         HttpSolrClient solr = new HttpSolrClient.Builder("http://localhost:8983/solr/pruebas3").build();
+        //String fileName = "C:\\Users\\antonio diego\\OneDrive - UNIVERSIDAD DE HUELVA\\Documentos\\UNI\\MB\\CISI\\CISI.QRY";
         String fileName = "C:\\Users\\antonio diego\\OneDrive - UNIVERSIDAD DE HUELVA\\Documentos\\UNI\\MB\\CISI\\CISI.QRY";
-        FileWriter trec = new FileWriter("C:\\Users\\antonio diego\\OneDrive - UNIVERSIDAD DE HUELVA\\Documentos\\UNI\\MB\\CISI\\trec.txt");
+        //FileWriter trec = new FileWriter("C:\\Users\\antonio diego\\OneDrive - UNIVERSIDAD DE HUELVA\\Documentos\\UNI\\MB\\CISI\\trec.txt");
+        FileWriter trec = new FileWriter("C:\\Users\\antonio diego\\OneDrive - UNIVERSIDAD DE HUELVA\\Documentos\\UNI\\MB\\CISI\\trec_AD.txt");
         PrintWriter escribir = new PrintWriter(trec);
         Scanner scan = new Scanner(new File(fileName));
         SolrQuery query = new SolrQuery();
         String consulta = "";
-        String indice="";
+        String indice = "";
+        Map<Integer, String> diccionario = new HashMap<Integer, String>();
+        diccionario = RellenarDiccionario(diccionario);
+
         while (scan.hasNextLine()) {
             String palabra = scan.nextLine();
-            if(palabra.contains(".I")){
+            if (palabra.contains(".I")) {
                 indice = palabra.substring(3);
             }
+
             if (palabra.equals(".W")) {
-                for (int i = 0; i < 5; i++) {
+                while (!scan.hasNext(".I") && !scan.hasNext(".B")) {
                     palabra = scan.next();
-                    if(palabra.startsWith("(")){
-                        palabra = palabra.substring(1);
-                    }
-                    if(palabra.endsWith(")")){
-                        palabra = palabra.substring(1,palabra.length()-1);
+                    for (int i = 0; i < palabra.length(); i++) {
+                        for (int j = 0; j < diccionario.size(); j++) {
+                            if(palabra.contains(diccionario.get(j))) {
+                                   palabra = palabra.replace(diccionario.get(j),"");
+                                }
+                        }
                     }
                     consulta += palabra + " ";
                 }
-                //System.out.println(consulta);
+                //System.out.println(indice);
+                //System.out.println(indice + " " + consulta);
                 query.setQuery("text:" + consulta);
-                query.setFields("score","id");
+                query.setFields("score", "id");
                 QueryResponse rsp = solr.query(query);
                 int ranking = 0;
                 SolrDocumentList docs = rsp.getResults();
                 for (int i = 0; i < docs.size(); ++i) {
-                    
+
                     escribir.println(indice + " Q0 " + docs.get(i).getFieldValue("id") + " " + ranking + " " + docs.get(i).getFieldValue("score") + " ETSI");
                     System.out.println(indice + " Q0 " + docs.get(i).getFieldValue("id") + " " + ranking + " " + docs.get(i).getFieldValue("score") + " ETSI");
                     ranking++;
@@ -63,5 +73,20 @@ public class Query {
             }
         }
         trec.close();
+    }
+
+    public static Map RellenarDiccionario(Map d) throws FileNotFoundException {
+        //String fileName = "C:\\Users\\antonio diego\\OneDrive - UNIVERSIDAD DE HUELVA\\Documentos\\UNI\\MB\\CISI\\Diccionario.txt";
+        String fileName = "C:\\Users\\antonio diego\\OneDrive - UNIVERSIDAD DE HUELVA\\Documentos\\UNI\\MB\\CISI\\Diccionario.txt";
+        Scanner scan = new Scanner(new File(fileName));
+        String palabra = "";
+        int i = 0;
+        while (scan.hasNextLine()) {
+            palabra = scan.nextLine();
+            d.put(i, palabra);
+            i++;
+        }
+        return d;
+
     }
 }
